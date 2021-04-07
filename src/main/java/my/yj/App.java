bb834f164c9a4424efaba78e24f28a30c09af0ac
Ye Jin Loh <yejinloh@gmail.com>
2021-04-07 11:59:23 +0800

    push

---
+++ src/main/java/my/yj/App.java
@@ -0,0 +1,23 @@
+package my.yj;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends JFrame
+{
+    public App(){
+        setTitle("Hello World!");
+        setSize(600,400);
+        setLocale(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+    }
+    public static void main( String[] args )
+    {
+        new App();
+    }
+}
